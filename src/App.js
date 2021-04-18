57f8ab88e6a19aa40a4590dfc68e5c3bb432cac1
Andrea Rosales <andrearosr@gmail.com>
2021-04-17 21:13:28 -0500

    Implement wheel with categories

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Wheel } from 'react-custom-roulette';
+import { categories } from './categories';
 import './App.css';
 
+const categoriesToWheel = () => categories.map(cat => ({ option: cat.emoji }));
+
 function App() {
+  const data = categoriesToWheel();
+  const [spin, setSpin] = useState(false);
+  const [roundClass, setRoundClass] = useState(null);
+
+  const handleSpinClick = () => {
+    setRoundClass(Math.floor(Math.random() * data.length));
+    setSpin(true);
+  }
+
+  const onStopSpinning = () => {
+    setSpin(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Cheat Me Now!</h1>
+      {spin && <h2>What will it be?</h2>}
+      {!spin && roundClass !== null && <h2>{categories[roundClass].label}</h2>}
+      <Wheel
+        mustStartSpinning={spin}
+        prizeNumber={roundClass}
+        data={data}
+        onStopSpinning={onStopSpinning}
+        fontSize={40}
+        textDistance={70}
+      />
+      <button onClick={handleSpinClick}>SPIN</button>
     </div>
   );
 }
---
+++ src/categories.js
@@ -0,0 +1,62 @@
+export const categories = [
+  {
+    class: 'Cat',
+    label: 'Cat meowing',
+    emoji: '😺',
+  },
+  {
+    class: 'Chainsaw',
+    label: 'Chainsaw',
+    emoji: '⚒️',
+  },
+  {
+    class: 'Snoring',
+    label: 'Person snoring',
+    emoji: '😴',
+  },
+  {
+    class: 'Cow',
+    label: 'Cow',
+    emoji: '🐮',
+  },
+  {
+    class: 'Crying',
+    label: 'Baby Crying',
+    emoji: '🍼',
+  },
+  {
+    class: 'Dog',
+    label: 'Dog barking',
+    emoji: '🐶',
+  },
+  {
+    class: 'Helicopter',
+    label: 'Helicopter',
+    emoji: '🚁',
+  },
+  {
+    class: 'Hen',
+    label: 'Hen',
+    emoji: '🥚',
+  },
+  {
+    class: 'Siren',
+    label: 'Siren',
+    emoji: '🚨',
+  },
+  {
+    class: 'Sneezing',
+    label: 'Person Sneezing',
+    emoji: '🤧',
+  },
+  {
+    class: 'Ticking',
+    label: 'Clock ticking',
+    emoji: '⏱️',
+  },
+  {
+    class: 'Rooster',
+    label: 'Rooster',
+    emoji: '🐓',
+  },
+];
